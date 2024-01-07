52e25b5ff254a4f4f89fc94d22441fbfbaf7d409
abdel-17 <abdelrahmanb015@gmail.com>
2024-01-07 02:52:58 +0200

    feat: add helpers

--- Sources/RationalModule/Rational.swift
+++ Sources/RationalModule/Rational.swift
@@ -8,14 +8,15 @@ public struct Rational<T: FixedWidthInteger & SignedInteger> {
 	/// Creates a rational number with the given numerator and denominator.
 	@inlinable
 	internal init(numerator: T, denominator: T) {
-		assert(denominator > 0, "Denominator must be positive.")
-		assert(gcd(numerator, denominator) == 1, "Fraction must normalized.")
+		assert(denominator > 0, "The denominator must be positive")
+		assert(gcd(numerator, denominator) == 1, "The fraction must normalized")
 
 		self.numerator = numerator
 		self.denominator = denominator
 	}
 }
 
+// MARK: - Initializers
 extension Rational {
 	/// Creates a rational number from a fraction.
 	///
@@ -31,10 +32,10 @@ extension Rational {
 	///   - numerator: The numerator of the fraction.
 	///   - denominator: The denominator of the fraction.
 	///
-	/// - Precondition: `denominator` must not be zero.
+	/// - Precondition: `denominator != 0`
 	@inlinable
 	public init(_ numerator: T, _ denominator: T) {
-		precondition(denominator != 0, "Denominator must not be zero.")
+		precondition(denominator != 0, "The denominator must not be zero")
 
 		// We cannot compute `gcd(0, T.min)` and `gcd(T.min, T.min)` due to overflow,
 		// so we must handle these cases separately.
@@ -58,5 +59,67 @@ extension Rational {
 		}
 
 		self.init(numerator: numerator, denominator: denominator)
+	}
+}
+
+// MARK: - Properties
+extension Rational {
+	/// The quotient of `numerator`divided by `denominator`
+	public var quotient: T {
+		numerator / denominator
+	}
+
+	/// The remainder of `numerator` divided by `denominator`.
+	public var remainder: T {
+		numerator % denominator
+	}
+
+	/// The quotient and remainder of `numerator` divided by `denominator`.
+	public var quotientAndRemainder: (quotient: T, remainder: T) {
+		numerator.quotientAndRemainder(dividingBy: denominator)
+	}
+}
+
+// MARK: - Helpers
+extension Rational {
+	/// Returns the numerator and denominator of this value as a tuple.
+	public func toRatio() -> (numerator: T, denominator: T) {
+		(numerator, denominator)
+	}
+
+	/// Returns the closest rational number to this value
+	/// with `denominator` at most `max`.
+	///
+	/// - Precondition: `max >= 1`
+	public func limitDenominator(max: T) -> Self {
+		precondition(max >= 1, "The value of `max` should be at least 1")
+
+		// Algorithm copied from cpython's fractions module.
+		// https://github.com/python/cpython/blob/main/Lib/fractions.py
+
+		guard denominator > max else { return self }
+
+		var p0: T = 0
+		var q0: T = 1
+		var p1: T = 1
+		var q1: T = 0
+		var n = numerator
+		var d = denominator
+
+		while true {
+			let a = n / d
+			let q2 = q0 + a * q1
+			guard q2 <= max else { break }
+
+			(p0, q0, p1, q1) = (p1, q1, p0 + a * p1, q2)
+			(n, d) = (d, n - a * d)
+		}
+
+		let k = (max - q0) / q1
+		if 2 * d * (q0 + k * q1) <= denominator {
+			return Self(numerator: p1, denominator: q1)
+		} else {
+			return Self(numerator: p0 + k * p1, denominator: q0 + k * q1)
+		}
 	}
 }
