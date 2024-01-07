2669a8986529d43907e1fc71e0eaaa9071138dfa
abdel-17 <abdelrahmanb015@gmail.com>
2024-01-07 16:28:22 +0200

    style: rename test names

---
+++ Tests/RationalModuleTests/HashableTests.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Abdelrahman Bahaa on 07/01/2024.
+//
+
+import Foundation
--- Tests/RationalModuleTests/StringConvertibleTests.swift
+++ Tests/RationalModuleTests/StringConvertibleTests.swift
@@ -3,22 +3,22 @@ import XCTest
 @testable import RationalModule
 
 final class StringConvertibleTests: XCTestCase {
-	func `test_one_over_two_string_conversion`() throws {
+	func `test_fraction_string_conversion`() throws {
 		let r = Rational(numerator: 1, denominator: 2)
 		XCTAssertEqual(r.description, "1/2")
 	}
 
-	func `test_one_over_one_string_conversion`() throws {
+	func `test_integer_string_conversion`() throws {
 		let r = Rational(numerator: 1, denominator: 1)
 		XCTAssertEqual(r.description, "1")
 	}
 
-	func `test_one_over_two_debug_string_conversion`() throws {
+	func `test_fraction_debug_string_conversion`() throws {
 		let r = Rational(numerator: 1, denominator: 2)
 		XCTAssertEqual(r.debugDescription, "Rational<Int>(1, 2)")
 	}
 
-	func `test_one_over_two_int32_debug_string_conversion`() throws {
+	func `test_fraction_int32_debug_string_conversion`() throws {
 		let r = Rational<Int32>(numerator: 1, denominator: 2)
 		XCTAssertEqual(r.debugDescription, "Rational<Int32>(1, 2)")
 	}
