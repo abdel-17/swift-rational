5761feae212ac0b237b3ff63150db9371d76309a
abdel-17 <abdelrahmanb015@gmail.com>
2024-01-08 23:20:20 +0200

    style: rename test method names

--- Tests/RationalModuleTests/FractionTests.swift
+++ Tests/RationalModuleTests/FractionTests.swift
@@ -3,37 +3,37 @@ import XCTest
 @testable import RationalModule
 
 final class FractionTests: XCTestCase {
-	func `test_two_over_four_equals_half`() throws {
+	func test_2_over_4_returns_1_over_2() throws {
 		let r = Rational(2, 4)
 		XCTAssertEqual(r.numerator, 1)
 		XCTAssertEqual(r.denominator, 2)
 	}
 
-	func `test_two_over_two_equals_one`() throws {
+	func test_2_over_2_returns_1_over_1() throws {
 		let r = Rational(2, 2)
 		XCTAssertEqual(r.numerator, 1)
 		XCTAssertEqual(r.denominator, 1)
 	}
 
-	func `test_one_over_negative_three_equals_negative_one_third`() throws {
+	func test_1_over_negative_3_returns_negative_1_over_3() throws {
 		let r = Rational(1, -3)
 		XCTAssertEqual(r.numerator, -1)
 		XCTAssertEqual(r.denominator, 3)
 	}
 
-	func `test_six_over_three_equals_two`() throws {
+	func test_6_over_3_returns_2_over_1() throws {
 		let r = Rational(6, 3)
 		XCTAssertEqual(r.numerator, 2)
 		XCTAssertEqual(r.denominator, 1)
 	}
 
-	func `test_zero_over_int_min_equals_zero`() throws {
+	func test_0_over_int_min_returns_0_over_1() throws {
 		let r = Rational(0, Int.min)
 		XCTAssertEqual(r.numerator, 0)
 		XCTAssertEqual(r.denominator, 1)
 	}
 
-	func `test_int_min_over_int_min_equals_one`() throws {
+	func test_int_min_over_int_min_returns_1_over_1() throws {
 		let r = Rational(Int.min, Int.min)
 		XCTAssertEqual(r.numerator, 1)
 		XCTAssertEqual(r.denominator, 1)
--- Tests/RationalModuleTests/IntegerLiteralTests.swift
+++ Tests/RationalModuleTests/IntegerLiteralTests.swift
@@ -3,7 +3,7 @@ import XCTest
 @testable import RationalModule
 
 final class IntegerLiteralTests: XCTestCase {
-	func `test_literal_two_conversion`() throws {
+	func test_literal_2_returns_2_over_1() throws {
 		let r: Rational<Int> = 2
 		XCTAssertEqual(r.numerator, 2)
 		XCTAssertEqual(r.denominator, 1)
